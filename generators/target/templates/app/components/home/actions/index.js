059c460b7dc9fba3f3083f82764488355bb36780
Charles Fulnecky <charles.fulnecky@hbo.com>
2017-01-19 20:14:37 -0500

    Bad copy paste, try again.

--- generators/target/templates/app/components/home/actions/index.js
+++
@@ -1,6 +0,0 @@
-// When action complexity increases create an 'actions' directory
-// Use actions/index.js to import individual actions
-// See reducers for an example
-
-// CRUD opererations
-
--- generators/target/templates/app/reducers.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import * as homeReducers from './components/home/reducers';
-
-const allReducers = Object.assign({}, homeReducers);
-
-export default combineReducers(allReducers);
